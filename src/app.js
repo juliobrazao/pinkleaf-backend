6865632c9dbf1d1f1014178cd9b9969c4ddeee3a
Julio Brazao <jotabrazao@gmail.com>
2023-08-13 21:06:09 -0400

    Adjusting entrypoint and cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { sum } = require('./functions/sum');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.status(200).json({
